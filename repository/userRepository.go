03980cb501a427843e5252488feaa5d04f2c5953
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-11-07 19:51:25 +0800

    feat: add edituser repository

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/arfandyam/Whisper-Me/models/domain"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -14,19 +15,37 @@ func NewUserRepository() UserRepositoryInterface {
 }
 
 func (repository *UserRepository) CreateUser(tx *gorm.DB, user *domain.User) (*domain.User, error) {
-	fmt.Println("user dari service", user)
-	fmt.Println("user.Id dari repo", user.Id)
-	fmt.Println("user.Password dari repo", *user.Password)
 	sql := "INSERT INTO users (id, username, firstname, lastname, email, password, oauth_id, is_oauth, is_verified, created_at, updated_at, deleted_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?) RETURNING id"
 	
-	rows := tx.Raw(sql, user.Id, user.Username, user.Firstname, user.Lastname, user.Email, *user.Password, user.Oauth_id, user.Is_oauth, user.Is_verified, nil, nil).Row()
+	rows := tx.Raw(sql, user.Id, user.Username, user.Firstname, user.Lastname, user.Email, &user.Password, user.Oauth_id, user.Is_oauth, user.Is_verified, nil, nil).Row()
 
-	fmt.Println("rows", &rows)
 	if err := rows.Scan(&user.Id); err != nil {
-		tx.Rollback()
 		return nil, err
-	} else {
-		tx.Commit()
-		return user, nil
 	}
+
+	return user, nil
+}
+
+func (repository *UserRepository) EditUser(tx *gorm.DB, user *domain.User) (*domain.User, error) {
+	fmt.Println("kocakedituser")
+	sql := "UPDATE users SET firstname = ?, lastname = ?, updated_at = NOW() WHERE id = ? RETURNING firstname, lastname"
+
+	rows := tx.Raw(sql, user.Firstname, user.Lastname, user.Id).Row()
+
+	if err := rows.Scan(&user.Firstname, &user.Lastname); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (repository *UserRepository) FindUserById(tx *gorm.DB, userId uuid.UUID) (*domain.User, error){
+	sql := "SELECT * FROM users WHERE id = ?"
+	user := &domain.User{}
+
+	if err := tx.Raw(sql, userId).Scan(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
--- repository/userRepositoryContract.go
+++ repository/userRepositoryContract.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"github.com/arfandyam/Whisper-Me/models/domain"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type UserRepositoryInterface interface {
 	CreateUser(tx *gorm.DB, user *domain.User) (*domain.User, error)
-	// EditUser(tx *gorm.DB, user domain.User) domain.User
-	// FindUserById(tx *gorm.DB, user domain.User) domain.User
+	EditUser(tx *gorm.DB, user *domain.User) (*domain.User, error)
+	FindUserById(tx *gorm.DB, userId uuid.UUID) (*domain.User, error)
 	// ChangePassword(tx *gorm.DB, user domain.User)
 }
