2696729238253c5f01261450ad3b032bef54d2f6
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-11-09 23:47:40 +0800

    feat: add auth route for login user

---
+++ router/auth/route.go
@@ -0,0 +1,13 @@
+package auth
+
+import (
+	"github.com/arfandyam/Whisper-Me/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(route *gin.Engine, authController controllers.AuthControllerInterface){
+    auth := route.Group("/auth")
+    {
+        auth.POST("", authController.LoginUser)
+    }
+}
