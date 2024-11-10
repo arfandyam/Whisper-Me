1b22e0f214b0518b7b4db64cb91c5fc3db25baa7
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-11-10 18:41:33 +0800

    feat: add refresh token req dto and access token res dto and additional iat and exp on auth res body

--- models/dto/authRequestBody.go
+++ models/dto/authRequestBody.go
@@ -4,3 +4,7 @@ type AuthRequestBody struct {
 	Username string `validate:"required" json:"username"`
 	Password string	`validate:"required,min=8" json:password`
 }
+
+type RefreshTokenRequestBody struct {
+	RefreshToken string `validate:"required" json:"refreshtoken"`
+}
--- models/dto/authResponseBody.go
+++ models/dto/authResponseBody.go
@@ -1,7 +1,18 @@
 package dto
 
+import "github.com/golang-jwt/jwt/v5"
+
 type AuthResponseBody struct {
 	*Response
-	AccessToken  string `json:"accesstoken"`
-	RefreshToken string	`json:"refreshtoken"`
+	AccessToken    string `json:"accesstoken"`
+	AccessTokenIat jwt.NumericDate `json:"accesstokeniat"`
+	AccessTokenExp jwt.NumericDate `json:"accesstokenexp"`
+	RefreshToken   string `json:"refreshtoken"`
+}
+
+type AccessTokenResponseBody struct {
+	*Response
+	AccessToken string `json:"accesstoken"`
+	AccessTokenIat jwt.NumericDate `json:"accesstokeniat"`
+	AccessTokenExp jwt.NumericDate `json:"accesstokenexp"`
 }
