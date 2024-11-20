cd5be38ea17216d009229835651eb7066f2c1f03
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-11-20 09:07:52 +0800

    style: adding white-space

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -5,14 +5,14 @@ import (
 	"github.com/arfandyam/Whisper-Me/models"
 )
 
-func init(){
+func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnDB()
 }
 
-func main(){
-	 /*Pisah AutoMigrate untuk tabel yg punya constraint karena dalam prosesnya tabel 
-	 terkadang belum terbuat tapi sudah ada constraint terhadap tabel lain*/
+func main() {
+	/*Pisah AutoMigrate untuk tabel yg punya constraint karena dalam prosesnya tabel
+	terkadang belum terbuat tapi sudah ada constraint terhadap tabel lain*/
 	initializers.DB.AutoMigrate(&models.User{}, &models.Authentication{})
 	initializers.DB.AutoMigrate(&models.Question{})
 	initializers.DB.AutoMigrate(&models.Response{})
