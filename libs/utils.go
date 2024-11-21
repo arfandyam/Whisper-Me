bf13c6c96c0e2bb4626ef783137ec5698081a604
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-11-21 10:32:34 +0800

    feat: create generate email body form template

--- libs/utils.go
+++ libs/utils.go
@@ -1,6 +1,9 @@
 package libs
 
 import (
+	"bytes"
+	"html/template"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -13,3 +16,18 @@ func CheckPasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
+
+func GenerateEmailBody(verificationLink string) (string, error){
+	tmpl, err := template.ParseFiles("../templates/emailVerification.html")
+	if err != nil {
+		return "", err
+	}
+
+	var rendered bytes.Buffer
+	if err := tmpl.Execute(&rendered, verificationLink); err != nil {
+		return "", err
+	}
+
+	return rendered.String(), nil
+}
+
