922c6569724af1500aa63bcce9be1bd7f518effa
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-11-23 17:58:10 +0800

    chore: add ToSlug function

--- libs/utils.go
+++ libs/utils.go
@@ -3,8 +3,9 @@ package libs
 import (
 	"bytes"
 	"html/template"
-
+	"strings"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -17,7 +18,7 @@ func CheckPasswordHash(password string, hash string) bool {
 	return err == nil
 }
 
-func GenerateEmailBody(verificationLink string) (string, error){
+func GenerateEmailBody(verificationLink string) (string, error) {
 	tmpl, err := template.ParseFiles("../templates/emailVerification.html")
 	if err != nil {
 		return "", err
@@ -31,3 +32,23 @@ func GenerateEmailBody(verificationLink string) (string, error){
 	return rendered.String(), nil
 }
 
+func ToSlug(itemString string, itemId uuid.UUID) string {
+
+	// lowercase and replace whitespace and underscore to hyphens
+	slug := strings.ToLower(itemString)
+	slug = strings.ReplaceAll(slug, " ", "-")
+	slug = strings.ReplaceAll(slug, "_", "-")
+
+	// Remove non-alphanumeric
+	var slugTrim strings.Builder
+	for i := 0; i < len(slug); i++ {
+		char := slug[i]
+		if ('a' <= char && char <= 'z') ||('A' <= char && char <= 'Z') ||('0' <= char && char <= '9') || char == ' ' || char == '-'{
+			slugTrim.WriteByte(char)
+		}
+	}
+
+	slug = slugTrim.String() + "-" + itemId.String()[:13]
+
+	return slug
+}
